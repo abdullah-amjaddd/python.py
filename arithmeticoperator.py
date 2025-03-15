ca56146427cb01d9dcd195960295fc65ab1724c7
abdullah-amjaddd <abdullazzak2005@gmail.com>
2025-03-15 18:16:08 +0500

    python

---
+++ arithmeticoperator.py
@@ -0,0 +1,9 @@
+a=20
+b=25
+print(a+b)#addition
+print(a-b)#subtraction
+print(a*b)#multiplication
+print(a/b)#division
+print(a%2)
+print(a**2)#exponent
+print(a//6)#floor division
---
+++ conditional statements.py
@@ -0,0 +1,17 @@
+# a=int(input("enter a even number:"))
+# if a%2==0 :
+#  print(a,"a is even number")
+# else :
+#   print(a,"a is an odd number")
+  #if elif statement
+b=int(input("enter the value of b"))
+if b>=90:
+    print("A grade")
+elif b>=80 :
+    print("b grade")
+elif b>=65 :
+    print("c grade")
+elif b<=60:
+    print("d grade")
+else :
+    print("fail")
